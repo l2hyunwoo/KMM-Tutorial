5a1200fb128feacaadc88aabe5cd2434baf3ed73
HyunWoo Lee <l2hyunwoo@gmail.com>
2022-12-28 14:22:42 +0900

    DB Driver 설정

--- shared/src/androidMain/kotlin/com/github/hyunwoo/todo/shared/cache/space/DatabaseDriverFactory.kt
+++ shared/src/androidMain/kotlin/com/github/hyunwoo/todo/shared/cache/space/DatabaseDriverFactory.kt
@@ -1,2 +1,17 @@
-package com.github.hyunwoo.todo.shared.cache.space 
+package com.github.hyunwoo.todo.shared.cache.space
 
+import android.content.Context
+import com.squareup.sqldelight.android.AndroidSqliteDriver
+import com.squareup.sqldelight.db.SqlDriver
+
+actual class DatabaseDriverFactory(
+    private val context: Context
+) {
+    actual fun createDriver(): SqlDriver {
+        return AndroidSqliteDriver(
+            schema = SpaceDatabase.Schema,
+            context = context,
+            name = "SpaceDatabase.db"
+        )
+    }
+}
--- shared/src/commonMain/kotlin/com/github/hyunwoo/todo/shared/cache/space/DatabaseDriverFactory.kt
+++ shared/src/commonMain/kotlin/com/github/hyunwoo/todo/shared/cache/space/DatabaseDriverFactory.kt
@@ -1,4 +1,7 @@
 package com.github.hyunwoo.todo.shared.cache.space
 
-class DatabaseDriverFactory {
+import com.squareup.sqldelight.db.SqlDriver
+
+expect class DatabaseDriverFactory {
+    fun createDriver(): SqlDriver
 }
